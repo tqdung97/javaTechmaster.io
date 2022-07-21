817c57a8662327bc934452c9f64e26679d4569ca
sonnguyenyhct <khacsonyhct@gmail.com>
2022-07-21 21:52:57 +0700

    [bai-tap]_buoi1_java

--- [bai-tap]-1_java/BaiTapBuoi1.java
+++ [bai-tap]-1_java/BaiTapBuoi1.java
@@ -6,15 +6,15 @@ public class BaiTapBuoi1 {
     public static void main(String[] args) {
         // Bài Tập 1
         int i = 10;
-        System.out.println(i);
+        System.out.println("i = "+ i);
 
         int n = i++ % 5;
-        System.out.println(n);
+        System.out.println("n = "+ n);
 
         int iInt = ++i;
-        System.out.println(iInt);
+        System.out.println("++i = " + iInt);
         int nInt = iInt % 5;
-        System.out.println(nInt);
+        System.out.println("So du :" + nInt);
 
         
         // Bài Tập 2
@@ -27,47 +27,51 @@ public class BaiTapBuoi1 {
 
         // bài tập 3
         double d = 9 / 5;
-        System.out.println(d);
+        System.out.println("d = " + d);
         double dDouble = (double) 9 / 5;
-        System.out.println(dDouble);
+        System.out.println("9 / 5 = " + dDouble);
 
 
         //Bài tập 4
          
         int x = 1221;
-        boolean check = true;
-        System.out.println(x);
+        int y = 3;
+        System.out.println("y = " + y);
+        System.out.println("x = " + x);
+
+
+        boolean check; 
+        
         if(x>0){
             check = true;
         }else check = false;
-        int y = 3;
-        System.out.println(y);
+        
 
-        System.out.println("Dk1:" + check) ;
+        System.out.println("Dk1: " + check) ;
         if(x % y == 0){
             check = true;
         }else check = false;
-        System.out.println("Dk2:" + check );
+        System.out.println("Dk2: " + check );
         if( 2 <= x && x <=15){
             check = true;
         }else check =false;
-        System.out.println("Dk3:" + check);
+        System.out.println("Dk3: " + check);
         if ( x - y < 5){
             check = true;
         }else check = false;
-        System.out.println("Dk4:" + check);
+        System.out.println("Dk4: " + check);
         if ( x > 30 && x < 10){
             check = true;
         }else check = false;
-        System.out.println("Dk5:" + check);
+        System.out.println("Dk5: " + check);
         if ( x / 1000 >= 1){
             check = true;
         }else check = false;
-        System.out.println("Dk6:" + check);
+        System.out.println("Dk6: " + check);
         if ( x / 100000 > 1  && x / 100000 < 9){
             check = true;
         }else check = false;
-        System.out.println("Dk6:" + check);
+        System.out.println("Dk7:" + check);
 
         }
     }
